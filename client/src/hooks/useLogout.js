a60349b6e30749dfc34685c08160db35c51c2a57
sc <stevencstuy@gmail.com>
2022-09-11 02:44:52 -0400

    Redirect to homepage after logout

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,15 +1,19 @@
 import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 export const useLogout = () => {
   // initialize dispatch function
   const { dispatch } = useContext(UserContext);
+  let navigate = useNavigate();
 
   // remove token from localStorage and dispatch LOGOUT action
   const logout = () => {
     localStorage.removeItem('token');
 
     dispatch({ type: 'LOGOUT' });
+
+    navigate('/');
   };
 
   return { logout };
