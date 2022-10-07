8ed0dd9e35b37566956dc15814f645488c168ad7
sc <stevencstuy@gmail.com>
2022-10-06 23:46:56 -0400

    Change navbar styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,23 +8,34 @@ export const Navbar = () => {
   const { logout } = useLogout();
 
   return (
-    <header className='flex justify-between h-32 items-center px-16 text-lg font-semibold tracking-wider'>
+    <header className='flex justify-between h-32 items-center px-16 tracking-wider'>
       <div>
-        <Link to='/'>Roverify</Link>
+        <Link to='/' className='text-lg font-semibold'>
+          Roverify
+        </Link>
       </div>
 
       {/* if user is logged in, show upload and log out button in navbar. otherwise, show log in and sign up */}
       {user ? (
         <div className='flex gap-12'>
-          <Link to='/upload'>Upload</Link>
-          <button onClick={() => logout()}>Log Out</button>
+          <Link to='/upload' className='text-gray-800 hover:text-black'>
+            Upload
+          </Link>
+          <button
+            className='text-gray-800 hover:text-black'
+            onClick={() => logout()}
+          >
+            Log Out
+          </button>
         </div>
       ) : (
         <div className='flex gap-8 items-center'>
-          <Link to='/login'>Login</Link>
+          <Link to='/login' className='text-gray-800 hover:text-black'>
+            Login
+          </Link>
           <Link
             to='/signup'
-            className='border border-black bg-slate-200 px-4 py-2 rounded-md text-black hover:bg-black hover:text-gray-200'
+            className='border border-black bg-black px-4 py-2 rounded-md text-white hover:bg-white hover:text-black'
           >
             Sign Up
           </Link>
